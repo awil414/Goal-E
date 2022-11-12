cc78475686c39ccbfa527d97087bc1df688fed41
Your Name <you@example.com>
2022-11-11 20:56:58 -0600

    Routes working for signup and login taken to profile

--- controllers/profileRoutes.js
+++ controllers/profileRoutes.js
@@ -1,35 +1,33 @@
 const router = require("express").Router();
-const { User, Goals, Checklist } = require("../../models");
-const withAuth = require("../../utils/auth");
+const { User, Goals, Checklist } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/', withAuth, async (req, res) => {
-    try {
-      const goalsData = await Goals.findAll({
-        attributes: ['id', 'title', 'description', 'created_at', 'user_id'],
-        include: [
-          {
-            model: Checklist,
-            attributes: ['id', 'description', 'created_at', 'goals_id'],
-          },
-        ],
-      });
-  
-      const goals = goalsData.map((goals) => goals.get({ plain: true })); 
-  
-      res.render('profile', {
-        goals,
-         logged_in: req.session.logged_in,
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const goalsData = await Goals.findAll({
+      where:{
+        user_id:req.session.user_id
+      },
+      attributes: ["id", "title", "description", "created_at", "user_id"],
+      include: [
+        {
+          model: Checklist,
+          attributes: ["id", "description", "created_at", "goals_id"],
+        },
+      ],
+    });
 
-  router.get('/goals', (req, res) => {
-    // If the user is already logged in, redirect the request to another route
-    if (req.session.logged_in) {
-      res.redirect('/');
-      return;
-    }
-    res.render('goals');
-  });
+    const goals = goalsData.map((goals) => goals.get({ plain: true }));
+
+    res.render("profile", {
+      goals,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+module.exports = router;
