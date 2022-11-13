f464ed4b350ab3945ce40b792ab3a8fbd2129cff
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-11-13 14:02:16 -0600

    adding editGoals.js functions

--- public/js/editGoal.js
+++ public/js/editGoal.js
@@ -2,7 +2,39 @@
 //method:"PUT"
 //document.location.replace("/profile") with edits showing on profile page
 
+const editGoalHandler = async (event) => {
+  event.preventDefault();
 
+  // Collect values from the post 
+  const title = document.querySelector('input[name="goal-title"]').value.trim();
+  const description = document
+    .querySelector('input[name="goal-steps"]')
+    .value.trim();
+
+  // Selects ONE post
+  const id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+
+  // Send a PUT request to the API endpoint
+  const response = await fetch(`/api/goals/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({ title, description }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    // If successful, redirect the browser to the dashboard page
+    document.location.replace("/profile");
+  } else {
+    alert(response.statusText);
+  }
+};
+
+document.querySelector("#editInput-button").addEventListener("click", editGoalHandler);
+
+
+// I don't think we need this. It is deleting goal already.
 //Button clicked to DELETE
 //method:"DELETE"
 //document.location.replace("/profile") with the deleted goal not showing
