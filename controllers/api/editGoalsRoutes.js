3fef7a450aeec793d499b188e184b2e0065a4408
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-11-14 12:53:47 -0600

    changes to editGoals.js, router, and handlebar for full function

--- controllers/api/editGoalsRoutes.js
+++ controllers/api/editGoalsRoutes.js
@@ -26,13 +26,15 @@ router.get('/edit/:id', async (req, res) => {
 });
 
 //Updating chosen goal
-router.put("edit/:id", withAuth, async (req, res) => {
+router.put("/:id", withAuth, async (req, res) => {
+  console.log(req.body)
   try {
     const goalsData = await Goals.update(req.body, {
       where: {
         id: req.params.id,
       },
     });
+    console.log(goalsData)
     if (!goalsData[0]) {
       res.status(404).json({ message: `No goals found with that id.` });
       return;
@@ -83,41 +85,41 @@ router.put("edit/:id", withAuth, async (req, res) => {
 // });
 
 //Updating steps for the goal
-router.put("/:id", withAuth, async (req, res) => {
-  try {
-    const checklistData = await Checklist.update(req.body, {
-      where: {
-        id: req.params.id,
-      },
-    });
-    if (!checklistData[0]) {
-      res.status(404).json({ message: `No checklist found with that id.` });
-      return;
-    }
-    res.status(200).json(checklistData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// router.put("/:id", withAuth, async (req, res) => {
+//   try {
+//     const checklistData = await Checklist.update(req.body, {
+//       where: {
+//         id: req.params.id,
+//       },
+//     });
+//     if (!checklistData[0]) {
+//       res.status(404).json({ message: `No checklist found with that id.` });
+//       return;
+//     }
+//     res.status(200).json(checklistData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
-//Deleting steps for chosen goal
-router.delete("/:id", withAuth, async (req, res) => {
-  try {
-    const checklistData = await Checklist.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-      },
-    });
-    if (!checklistData) {
-      res.status(404).json({ message: "No checklist found with this id!" });
-      return;
-    }
-    res.status(200).json(checklistData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// //Deleting steps for chosen goal
+// router.delete("/:id", withAuth, async (req, res) => {
+//   try {
+//     const checklistData = await Checklist.destroy({
+//       where: {
+//         id: req.params.id,
+//         user_id: req.session.user_id,
+//       },
+//     });
+//     if (!checklistData) {
+//       res.status(404).json({ message: "No checklist found with this id!" });
+//       return;
+//     }
+//     res.status(200).json(checklistData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 
 
